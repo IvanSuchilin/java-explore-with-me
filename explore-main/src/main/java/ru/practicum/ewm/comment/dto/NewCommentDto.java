f35ab1b9a50d10dc78f9aa3ab4b27e7ab2900896
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2023-02-14 17:05:39 +0300

    added comment rep, service, model, dto, endpoints

---
+++ explore-main/src/main/java/ru/practicum/ewm/comment/dto/NewCommentDto.java
@@ -0,0 +1,15 @@
+package ru.practicum.ewm.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewCommentDto {
+        private Long eventId;
+        private String text;
+}
